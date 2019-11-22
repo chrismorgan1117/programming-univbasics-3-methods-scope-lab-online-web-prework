7e777f3054ae553c58a3da3f74af331043ad9e0b
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 05:37:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,13 @@
 
 
 def mario
-  
-  puts phrase = "It's-a me, Mario!"
+  phrase = "It's-a me, Mario"
+  puts phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+def link
+  phra
